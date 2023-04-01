9cbb1970ca263ae055e1bb3759841b02812c6e8c
shashi675 <shashikantrai675@gmail.com>
2023-04-01 19:13:53 +0530

    frontend completed, bubble, selection, intersion, merge, heap sorts functionality added

---
+++ js/visualise.js
@@ -0,0 +1,34 @@
+
+function compare(a, b) {
+    div[a].style.backgroundColor = "orange";
+    div[b].style.backgroundColor = "orange";
+}
+
+function swap(a, b) {
+    // swapping heights of divs:
+    div[a].style.height = height[b] + "%";
+    div[b].style.height = height[a] + "%";
+
+    // actual swapping:
+    // height[a] = height[a] + height[b];
+    // height[b] = height[a] - height[b];
+    // height[a] = height[a] - height[b];
+
+    const temp=height[a];
+    height[a]=height[b];
+    height[b]=temp;
+}
+
+function updateColor(a, color) {
+    div[a].style.backgroundColor = color;
+}
+
+function sleep() {
+    return new Promise((resolve, reject) => setTimeout(() => {
+        resolve();
+    }, time));
+}
+
+function updateHeight(a) {
+    div[a].style.height = height[a] + "%";
+}
